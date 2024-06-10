9d0420cfddb2118411ba491e3b24fd7f16142c9d
Rafael Silva <ainbot6@gmail.com>
2024-06-10 20:42:07 -0300

    feat: add extensions

---
+++ AuthenticateAPI/Extensions/DatabaseDependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using AuthenticateAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenticateAPI.Extensions;
+
+public static class DatabaseDependencyInjection
+{
+    public static IServiceCollection AddDatabaseDependencyInjection(this IServiceCollection service,
+        IConfiguration configuration)
+    {
+        service.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                x => x.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
+
+        return service;
+    }
+}
---
+++ AuthenticateAPI/Extensions/FluentValidationDependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using AuthenticateAPI.Dto.Request;
+using AuthenticateAPI.Dto.Response;
+using AuthenticateAPI.FluentValidations.Dto.Request;
+using AuthenticateAPI.FluentValidations.Dto.Response;
+using AuthenticateAPI.FluentValidations.Models;
+using AuthenticateAPI.Models;
+using FluentValidation;
+
+namespace AuthenticateAPI.Extensions;
+
+public static class FluentValidationDependencyInjection
+{
+    public static IServiceCollection AddFluentValidationDependencyInjection(this IServiceCollection service)
+    {
+        service.AddValidatorsFromAssemblyContaining<ChangePasswordDtoRequestValidator>();
+        service.AddScoped<IValidator<ChangePasswordDtoRequest>, ChangePasswordDtoRequestValidator>();
+        
+        service.AddValidatorsFromAssemblyContaining<LoginDtoRequestValidator>();
+        service.AddScoped<IValidator<LoginDtoRequest>, LoginDtoRequestValidator>();
+        
+        service.AddValidatorsFromAssemblyContaining<UpdateUserDtoRequestValidator>();
+        service.AddScoped<IValidator<UpdateUserDtoRequest>, UpdateUserDtoRequestValidator>();
+
+        service.AddValidatorsFromAssemblyContaining<AuthenticatedDtoResponseValidator>();
+        service.AddScoped<IValidator<AuthenticatedDtoResponse>, AuthenticatedDtoResponseValidator>();
+
+        service.AddValidatorsFromAssemblyContaining<RegisteredDtoResponseValidator>();
+        service.AddScoped<IValidator<RegisteredDtoResponse>, RegisteredDtoResponseValidator>();
+
+        service.AddValidatorsFromAssemblyContaining<UserValidator>();
+        service.AddScoped<IValidator<User>, UserValidator>();
+
+        return service;
+    }
+}
