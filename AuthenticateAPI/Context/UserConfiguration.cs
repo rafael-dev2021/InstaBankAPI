f9992fb168954196ad92d5fedd542711c03e2cda
Rafael Silva <ainbot6@gmail.com>
2024-06-10 18:36:42 -0300

    feat: add ef core configuration

---
+++ AuthenticateAPI/Context/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using AuthenticateAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthenticateAPI.Context;
+
+public class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(x => x.Id);
+        builder.Property(x => x.Name).HasMaxLength(50).IsRequired();
+        builder.Property(x => x.LastName).HasMaxLength(50).IsRequired();
+        builder.Property(x => x.Email).HasMaxLength(50).IsRequired();
+        builder.Property(x => x.PhoneNumber).HasMaxLength(14).IsRequired();
+        builder.Property(x => x.Cpf).HasMaxLength(14).IsRequired();
+    }
+}
