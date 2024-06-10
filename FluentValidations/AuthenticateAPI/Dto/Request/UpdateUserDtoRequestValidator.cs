bd0c5e74f6896d7ab5110032263beda3e2d44e57
Rafael Silva <ainbot6@gmail.com>
2024-06-10 19:45:19 -0300

    refactor: remove duplicate codes

--- FluentValidations/AuthenticateAPI/Dto/Request/UpdateUserDtoRequestValidator.cs
+++ FluentValidations/AuthenticateAPI/Dto/Request/UpdateUserDtoRequestValidator.cs
@@ -7,25 +7,9 @@ public class UpdateUserDtoRequestValidator : AbstractValidator<UpdateUserDtoRequ
 {
     public UpdateUserDtoRequestValidator()
     {
-        RuleFor(x => x.Name)
-            .NotEmpty()
-            .WithMessage("Name is required.")
-            .Length(2, 50).WithMessage("Name must be between 2 and 50 characters long.");
-
-        RuleFor(x => x.LastName)
-            .NotEmpty()
-            .WithMessage("Last name is required.")
-            .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters long.");
-        
-        RuleFor(x => x.Email)
-            .NotEmpty()
-            .WithMessage("Email is required.")
-            .EmailAddress()
-            .WithMessage("Invalid email format.");
-
-        RuleFor(x => x.PhoneNumber)
-            .Matches(@"^\+?[0-9]{1,15}$")
-            .WithMessage("Invalid phone number format.")
-            .When(x => !string.IsNullOrWhiteSpace(x.PhoneNumber));
+        RuleFor(x => x.Name)!.NameRules();
+        RuleFor(x => x.LastName)!.LastNameRules();
+        RuleFor(x => x.Email)!.EmailRules();
+        RuleFor(x => x.PhoneNumber).PhoneNumberRules(x => x.PhoneNumber);
     }
 }
--- FluentValidations/AuthenticateAPI/Models/UserValidator.cs
+++ FluentValidations/AuthenticateAPI/Models/UserValidator.cs
@@ -7,29 +7,10 @@ public class UserValidator : AbstractValidator<User>
 {
     public UserValidator()
     {
-        RuleFor(x => x.Name)
-            .NotEmpty()
-            .WithMessage("Name is required.")
-            .Length(2, 50).WithMessage("Name must be between 2 and 50 characters long.");
-
-        RuleFor(x => x.LastName)
-            .NotEmpty()
-            .WithMessage("Last name is required.")
-            .Length(2, 50).WithMessage("Last name must be between 2 and 50 characters long.");
-        
-        RuleFor(x => x.Email)
-            .NotEmpty()
-            .WithMessage("Email is required.")
-            .EmailAddress()
-            .WithMessage("Invalid email format.");
-
-        RuleFor(x => x.PhoneNumber)
-            .Matches(@"^\+?[0-9]{1,15}$")
-            .WithMessage("Invalid phone number format.")
-            .When(x => !string.IsNullOrWhiteSpace(x.PhoneNumber));
-        
-        RuleFor(x => x.Cpf)
-            .Matches(@"^\d{3}\.\d{3}\.\d{3}\-\d{2}$")
-            .WithMessage("Invalid CPF format.");
+        RuleFor(x => x.Name)!.NameRules();
+        RuleFor(x => x.LastName)!.LastNameRules();
+        RuleFor(x => x.Email)!.EmailRules();
+        RuleFor(x => x.PhoneNumber).PhoneNumberRules(x=>x.PhoneNumber);
+        RuleFor(x => x.Cpf).CpfRules();
     }
 }
