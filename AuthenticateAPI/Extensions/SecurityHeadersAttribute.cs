d161e8479a41e12657c1d1ad4f41dba328850706
Rafael Silva <ainbot6@gmail.com>
2024-06-12 17:03:39 -0300

    refactor: remove the unnecessary Boolean literal

--- AuthenticateAPI/Extensions/SecurityHeadersAttribute.cs
+++ AuthenticateAPI/Extensions/SecurityHeadersAttribute.cs
@@ -6,22 +6,22 @@ public class SecurityHeadersAttribute : ActionFilterAttribute
 {
     public override void OnResultExecuting(ResultExecutingContext context)
     {
-        if (context.HttpContext.Response.Headers.ContainsKey("X-Content-Type-Options") == false)
+        if (!context.HttpContext.Response.Headers.ContainsKey("X-Content-Type-Options"))
         {
             context.HttpContext.Response.Headers.Append("X-Content-Type-Options", "nosniff");
         }
 
-        if (context.HttpContext.Response.Headers.ContainsKey("X-Frame-Options") == false)
+        if (!context.HttpContext.Response.Headers.ContainsKey("X-Frame-Options"))
         {
             context.HttpContext.Response.Headers.Append("X-Frame-Options", "SAMEORIGIN");
         }
 
-        if (context.HttpContext.Response.Headers.ContainsKey("X-XSS-Protection") == false)
+        if (!context.HttpContext.Response.Headers.ContainsKey("X-XSS-Protection"))
         {
             context.HttpContext.Response.Headers.Append("X-XSS-Protection", "1; mode=block");
         }
 
-        if (context.HttpContext.Response.Headers.ContainsKey("Referrer-Policy") == false)
+        if (!context.HttpContext.Response.Headers.ContainsKey("Referrer-Policy"))
         {
             context.HttpContext.Response.Headers.Append("Referrer-Policy", "no-referrer");
         }
