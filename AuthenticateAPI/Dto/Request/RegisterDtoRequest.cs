124d4e8a5098c40f07e157acfcfde158b9f4e736
Rafael Silva <ainbot6@gmail.com>
2024-06-11 23:19:19 -0300

    feat: add new dtos

---
+++ AuthenticateAPI/Dto/Request/RegisterDtoRequest.cs
@@ -0,0 +1,10 @@
+namespace AuthenticateAPI.Dto.Request;
+
+public record RegisterDtoRequest(
+    string Name,
+    string LastName,
+    string PhoneNumber,
+    string Cpf,
+    string Email,
+    string Password,
+    string ConfirmPassword) { }
