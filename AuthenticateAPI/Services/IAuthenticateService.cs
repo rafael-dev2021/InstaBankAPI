dc51f5a82b39aab91352f582f3a094ad251ff56e
Rafael Silva <ainbot6@gmail.com>
2024-06-14 20:26:18 -0300

    feat: add new interface

---
+++ AuthenticateAPI/Services/IAuthenticateService.cs
@@ -0,0 +1,12 @@
+﻿using AuthenticateAPI.Dto.Request;
+using AuthenticateAPI.Dto.Response;
+
+namespace AuthenticateAPI.Services;
+
+public interface IAuthenticateService
+{
+    Task<TokenDtoResponse> LoginAsync(LoginDtoRequest request);
+    Task<TokenDtoResponse> RegisterAsync(RegisterDtoRequest request);
+    Task<TokenDtoResponse> UpdateAsync(UpdateUserDtoRequest request, string userId);
+    Task<bool> ChangePasswordAsync(ChangePasswordDtoRequest request);
+}
