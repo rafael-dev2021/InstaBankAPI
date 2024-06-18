542fc43700cf243811882063977e3ebbc91fe713
Rafael Silva <ainbot6@gmail.com>
2024-06-18 19:29:30 -0300

    feat: add new service

--- AuthenticateAPI/Services/IAuthenticateService.cs
+++ AuthenticateAPI/Services/IAuthenticateService.cs
@@ -5,6 +5,7 @@ namespace AuthenticateAPI.Services;
 
 public interface IAuthenticateService
 {
+    Task<IEnumerable<UserDtoResponse>> GetAllUsersDtoAsync();
     Task<TokenDtoResponse> LoginAsync(LoginDtoRequest request);
     Task<TokenDtoResponse> RegisterAsync(RegisterDtoRequest request);
     Task<TokenDtoResponse> UpdateAsync(UpdateUserDtoRequest request, string userId);
