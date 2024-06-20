53e7a1e8547285b9a45c9a4f2c91c9689572d0cc
Rafael Silva <ainbot6@gmail.com>
2024-06-20 00:01:20 -0300

    feat: add new entity type configuration

---
+++ AuthenticateAPI/Context/TokenConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using AuthenticateAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AuthenticateAPI.Context;
+
+public class TokenConfiguration : IEntityTypeConfiguration<Token>
+{
+    public void Configure(EntityTypeBuilder<Token> builder)
+    {
+        builder.HasKey(t => t.Id);
+        builder.Property(t => t.TokenValue);
+        builder.Property(t => t.TokenRevoked);
+        builder.Property(t => t.TokenExpired);
+        builder.Property(t => t.TokenType);
+        
+        builder.HasOne(t => t.User)
+            .WithMany(u => u.Tokens)
+            .HasForeignKey(t => t.UserId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
