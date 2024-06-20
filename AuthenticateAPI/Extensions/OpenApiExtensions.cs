5d25c6789e4e65dfc1c1c5c2e7911cf4e776ee59
Rafael Silva <ainbot6@gmail.com>
2024-06-19 21:58:06 -0300

    feat: add open api extension

---
+++ AuthenticateAPI/Extensions/OpenApiExtensions.cs
@@ -0,0 +1,76 @@
+﻿using System.Reflection;
+using Microsoft.OpenApi.Models;
+
+namespace AuthenticateAPI.Extensions;
+
+public static class OpenApiExtensions
+{
+    public static void AddOpenApiExtensions(this IServiceCollection services)
+    {
+        services.AddAuthorizationBuilder()
+            .AddPolicy("Admin", policy => { policy.RequireRole("Admin"); });
+
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "AuthenticateAPI",
+                Description = "An ASP.NET Core Web API for managing AuthenticateAPI",
+                TermsOfService = new Uri("https://github.com/rafael-dev2021"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Rafael Dev",
+                    Email = "Rafael98kk@gmail.com",
+                    Url = new Uri("https://github.com/rafael-dev2021")
+                }
+            });
+
+            c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                Description = @"'Bearer' [space] your token",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer"
+            });
+
+            c.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        },
+                        Scheme = "oauth2",
+                        Name = "Bearer",
+                        In = ParameterLocation.Header
+                    },
+                    new List<string>()
+                }
+            });
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+
+        services.AddAuthorizationBuilder()
+            .AddPolicy("ApiScope", policy =>
+            {
+                policy.RequireAuthenticatedUser();
+                policy.RequireClaim("scope", "AuthenticateAPI");
+            });
+    }
+}
