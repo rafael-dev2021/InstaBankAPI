cc5977141d3ace2d5f56180d3ee659c88a32f8a4
Rafael Silva <ainbot6@gmail.com>
2024-06-22 01:26:57 -0300

    feat: add new validator

---
+++ AuthenticateAPI/FluentValidations/Dto/Request/RefreshTokenDtoRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using AuthenticateAPI.Dto.Request;
+using FluentValidation;
+
+namespace AuthenticateAPI.FluentValidations.Dto.Request;
+
+public class RefreshTokenDtoRequestValidator : AbstractValidator<RefreshTokenDtoRequest>
+{
+    public RefreshTokenDtoRequestValidator()
+    {
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty()
+            .WithMessage("Refresh token is required.");
+    }
+}
