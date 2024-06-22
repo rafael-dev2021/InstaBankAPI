3f6c2dbe162d4874680e260e0f25f3ab12afad2e
Rafael Silva <ainbot6@gmail.com>
2024-06-22 03:47:33 -0300

    feat: add extensions

---
+++ BankingServiceAPI/Extensions/FluentValidationDependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using BankingServiceAPI.FluentValidations.Models;
+using BankingServiceAPI.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+
+namespace BankingServiceAPI.Extensions;
+
+public static class FluentValidationDependencyInjection
+{
+    public static void AddFluentValidationDependencyInjection(this IServiceCollection service)
+    {
+        service.AddValidatorsFromAssemblyContaining<BaseEntityValidator>();
+        service.AddScoped<IValidator<BaseEntity>, BaseEntityValidator>();
+        
+        service.AddValidatorsFromAssemblyContaining<IndividualAccountValidator>();
+        service.AddScoped<IValidator<IndividualAccount>, IndividualAccountValidator>();
+
+        service.AddValidatorsFromAssemblyContaining<CorporateAccountValidator>();
+        service.AddScoped<IValidator<CorporateAccount>, CorporateAccountValidator>();
+        
+        service.AddValidatorsFromAssemblyContaining<AddressValidator>();
+        service.AddScoped<IValidator<Address>, AddressValidator>();
+        
+        service.AddFluentValidationAutoValidation();
+        service.AddFluentValidationClientsideAdapters();
+    }
+}
---
+++ BankingServiceAPI/Extensions/InfrastructureModule.cs
@@ -0,0 +1,10 @@
+﻿namespace BankingServiceAPI.Extensions;
+
+public static class InfrastructureModule
+{
+    public static void AddInfrastructureModule(this IServiceCollection service)
+    {
+        service.AddDatabaseDependencyInjection();
+        service.AddFluentValidationDependencyInjection();
+    }
+}
