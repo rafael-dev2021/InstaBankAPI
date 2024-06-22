7ed80df48e0661c447577e645922afb5fb1214f3
Rafael Silva <ainbot6@gmail.com>
2024-06-22 04:16:07 -0300

    feat: add new dependency injection

---
+++ BankingServiceAPI/Extensions/DependencyInjectionRepositories.cs
@@ -0,0 +1,15 @@
+﻿using BankingServiceAPI.Repositories;
+using BankingServiceAPI.Repositories.Interfaces;
+
+namespace BankingServiceAPI.Extensions;
+
+public static class DependencyInjectionRepositories
+{
+    public static void AddDependencyInjectionRepositories(this IServiceCollection service)
+    {
+        service.AddScoped<IBaseEntityRepository, IBaseEntityRepository>();
+        service.AddScoped<IIndividualAccountRepository, IndividualAccountRepository>();
+        service.AddScoped<ICorporateAccountRepository, CorporateAccountRepository>();
+        service.AddScoped<IAddressRepository, AddressRepository>();
+    }
+}
