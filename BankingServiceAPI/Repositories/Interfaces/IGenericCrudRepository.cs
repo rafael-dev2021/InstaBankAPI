f02e4ca9a3abffdf3500d7cb85ea78e8a57b1971
Rafael Silva <ainbot6@gmail.com>
2024-06-24 03:24:07 -0300

    refactor: small change

--- BankingServiceAPI/Repositories/Interfaces/IGenericCrudRepository.cs
+++ BankingServiceAPI/Repositories/Interfaces/IGenericCrudRepository.cs
@@ -3,8 +3,7 @@
 public interface IGenericCrudRepository<T> where T : class
 {
     Task<IEnumerable<T>> GetEntitiesAsync();
-    Task<T> GetByIdAsync(int? id);
-    Task<T> CreateAsync(T entity);
-    Task<T> UpdateAsync(T entity);
-    Task<T> DeleteAsync(T entity);
+    Task<T?> GetEntityByIdAsync(int? id);
+    Task<T> CreateEntityAsync(T entity);
+    Task<bool> DeleteEntityAsync(int? id);
 }
