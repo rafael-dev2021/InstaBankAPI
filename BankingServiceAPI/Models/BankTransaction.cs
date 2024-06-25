6f6c8105d218072d13d3b069bb33a1ff40f00bb1
Rafael Silva <ainbot6@gmail.com>
2024-06-25 00:12:49 -0300

    feat: add new models

---
+++ BankingServiceAPI/Models/BankTransaction.cs
@@ -0,0 +1,20 @@
+﻿namespace BankingServiceAPI.Models;
+
+public class BankTransaction
+{
+    public int Id { get; protected set; }
+    public BankAccount? AccountOrigin { get; protected set; }
+    public int AccountOriginId { get; protected set; }
+    public BankAccount? AccountDestination { get; protected set; }
+    public int AccountDestinationId { get; protected set; }
+    public decimal Amount { get; protected set; }
+    public DateTime TransferDate { get; protected set; }
+
+    public void SetId(int id) => Id = id;
+    public void SetAccountOrigin(BankAccount accountOrigin) => AccountOrigin = accountOrigin;
+    public void SetAccountOriginId(int accountOriginId) => AccountOriginId = accountOriginId;
+    public void SetAccountDestination(BankAccount accountDestination) => AccountDestination = accountDestination;
+    public void SetAccountDestinationId(int accountDestinationId) => AccountDestinationId = accountDestinationId;
+    public void SetAmount(decimal amount) => Amount = amount;
+    public void SetTransferDate(DateTime transferDate) => TransferDate = transferDate;
+}
---
+++ BankingServiceAPI/Models/Deposit.cs
@@ -0,0 +1,14 @@
+﻿namespace BankingServiceAPI.Models;
+
+public sealed class Deposit : BankTransaction
+{
+    public void Execute()
+    {
+        if (AccountOrigin == null)
+        {
+            throw new InvalidOperationException("Origin account is required for deposit.");
+        }
+
+        AccountOrigin.SetBalance(AccountOrigin.Balance + Amount);
+    }
+}
