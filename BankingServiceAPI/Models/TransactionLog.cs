71c327586dce3c71034edefa7e7267ac1fe610e5
Rafael Silva <ainbot6@gmail.com>
2024-06-25 00:35:27 -0300

    feat: add new model

---
+++ BankingServiceAPI/Models/TransactionLog.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingServiceAPI.Models;
+
+public class TransactionLog
+{
+    public int TransactionId { get; set; }
+    public BankTransaction? BankTransaction { get; set; }
+    public int BankTransactionId { get; set; }
+    public DateTime TimeDate { get; set; } = DateTime.Now;
+}
