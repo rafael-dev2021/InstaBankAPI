c243485a13e0ba2939dcd47ae291ba66a5a652ae
Rafael Silva <ainbot6@gmail.com>
2024-06-25 03:15:35 -0300

    feat: add new fluent validations

---
+++ BankingServiceAPI/FluentValidations/Dto/Request/DepositDtoRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using BankingServiceAPI.Dto.Request;
+using FluentValidation;
+
+namespace BankingServiceAPI.FluentValidations.Dto.Request;
+
+public class DepositDtoRequestValidator: AbstractValidator<DepositDtoRequest>
+{
+    public DepositDtoRequestValidator()
+    {
+        RuleFor(x => x.AccountNumber)
+            .GreaterThan(0)
+            .WithMessage("Account number must be greater than zero");
+        
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .WithMessage("Transfer amount must be greater than zero.");
+    }
+}
---
+++ BankingServiceAPI/FluentValidations/Dto/Request/WithdrawDtoRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using BankingServiceAPI.Dto.Request;
+using FluentValidation;
+
+namespace BankingServiceAPI.FluentValidations.Dto.Request;
+
+public class WithdrawDtoRequestValidator : AbstractValidator<WithdrawDtoRequest>
+{
+    public WithdrawDtoRequestValidator()
+    {
+        RuleFor(x => x.AccountNumber)
+            .GreaterThan(0)
+            .WithMessage("Account number must be greater than 0.");
+
+        RuleFor(x => x.Amount)
+            .GreaterThan(0)
+            .WithMessage("Amount must be greater than 0.");
+    }
+}
