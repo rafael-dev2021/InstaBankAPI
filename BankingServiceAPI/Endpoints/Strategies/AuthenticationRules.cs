b8be28f764d3d1e3a85b597cfcee9dd5f2a6736c
Rafael Silva <ainbot6@gmail.com>
2024-06-25 19:05:44 -0300

    feat: add new strategy

---
+++ BankingServiceAPI/Endpoints/Strategies/AuthenticationRules.cs
@@ -0,0 +1,28 @@
+﻿namespace BankingServiceAPI.Endpoints.Strategies;
+
+public static class AuthenticationRules
+{
+    public static IResult? CheckAuthenticationAndAuthorization(HttpContext context, bool requireAuthentication)
+    {
+        if (!requireAuthentication) return null;
+
+        if (!context.User.Identity!.IsAuthenticated)
+        {
+            var errorResponse = new Dictionary<string, string> { { "Message", "User is not authenticated." } };
+            return Results.Json(errorResponse, statusCode: StatusCodes.Status401Unauthorized);
+        }
+
+        if (context.User.IsInRole("Admin")) return null;
+        {
+            var errorResponse = new Dictionary<string, string>
+                { { "Message", "User does not have the required authorization." } };
+            return Results.Json(errorResponse, statusCode: StatusCodes.Status403Forbidden);
+        }
+    }
+
+    public static IResult HandleUnauthorizedAccessException(UnauthorizedAccessException ex)
+    {
+        var errorResponse = new Dictionary<string, string> { { "Message", ex.Message } };
+        return Results.Json(errorResponse, statusCode: StatusCodes.Status403Forbidden);
+    }
+}
