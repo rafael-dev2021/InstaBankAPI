97a4c5e1bd26581695fe3e623e7d7f07ee48f635
Rafael Silva <ainbot6@gmail.com>
2024-06-26 18:01:58 -0300

    feat: add new mappers profiles

---
+++ BankingServiceAPI/Mapper/MappingTheProfileDeposit.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using BankingServiceAPI.Dto.Response;
+using BankingServiceAPI.Models;
+
+namespace BankingServiceAPI.Mapper;
+
+public class MappingTheProfileDeposit : Profile
+{
+    public MappingTheProfileDeposit()
+    {
+        CreateMap<Deposit, DepositDtoResponse>()
+            .ConstructUsing(src => new DepositDtoResponse(
+                src.Id,
+                src.AccountDestination!.User!.Name!,
+                src.AccountDestination.User.LastName!,
+                src.AccountDestination.User.Cpf!,
+                src.AccountDestination.AccountNumber,
+                src.Amount,
+                src.TransferDate
+            ));
+    }
+}
---
+++ BankingServiceAPI/Mapper/MappingTheTransferProfile.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BankingServiceAPI.Dto.Response;
+using BankingServiceAPI.Models;
+
+namespace BankingServiceAPI.Mapper;
+
+public class MappingTheTransferProfile : Profile
+{
+    public MappingTheTransferProfile()
+    {
+        CreateMap<Transfer, TransferDtoResponse>()
+            .ConstructUsing(src => new TransferDtoResponse(
+                src.Id,
+                src.AccountDestination!.User!.Name!,
+                src.AccountDestination.User.LastName!,
+                src.AccountDestination.User.Cpf!,
+                src.Amount,
+                src.TransferDate
+            ));
+
+        CreateMap<Transfer, TransferByBankAccountNumberDtoResponse>()
+            .ConstructUsing(src => new TransferByBankAccountNumberDtoResponse(
+                src.Id,
+                src.AccountDestination!.User!.Name!,
+                src.AccountDestination.User.LastName!,
+                src.AccountDestination.AccountNumber,
+                src.Amount,
+                src.TransferDate
+            ));
+    }
+}
