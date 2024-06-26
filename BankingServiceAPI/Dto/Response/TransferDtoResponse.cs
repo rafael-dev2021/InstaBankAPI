dd2cfe051a1192bbe9a05ec8448b2a9e7fc5b6dc
Rafael Silva <ainbot6@gmail.com>
2024-06-26 17:58:33 -0300

    feat: add new dtos response

---
+++ BankingServiceAPI/Dto/Response/TransferDtoResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace BankingServiceAPI.Dto.Response;
+
+public record TransferDtoResponse(
+    int TransactionId,
+    string Name,
+    string LastName,
+    string DestinationCpf,
+    decimal Amount,
+    DateTime TransferDate);
