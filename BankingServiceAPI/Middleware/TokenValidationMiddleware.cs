1fd9fcf3e53ce4da16bac89dcb198d6661f016cb
Rafael Silva <ainbot6@gmail.com>
2024-06-27 17:08:26 -0300

    feat: add new method IsExpiredTokenValidAsync

--- BankingServiceAPI/Middleware/TokenValidationMiddleware.cs
+++ BankingServiceAPI/Middleware/TokenValidationMiddleware.cs
@@ -4,14 +4,17 @@ namespace BankingServiceAPI.Middleware;
 
 public class TokenValidationMiddleware(RequestDelegate next, HttpClient httpClient)
 {
+    private const string BaseUrl = "https://localhost:7074";
+
     public async Task InvokeAsync(HttpContext context)
     {
         if (context.Request.Headers.TryGetValue("Authorization", out var token))
         {
             token = token.ToString().Replace("Bearer ", string.Empty);
-            var isValid = await IsTokenValidAsync(token!);
+            var isRevokedTokenValidAsync = await IsRevokedTokenValidAsync(token!);
+            var isExpiredTokenValidAsync = await IsExpiredTokenValidAsync(token!);
 
-            if (!isValid)
+            if (!isRevokedTokenValidAsync || !isExpiredTokenValidAsync)
             {
                 context.Response.StatusCode = StatusCodes.Status401Unauthorized;
                 await context.Response.WriteAsync("Token is expired or revoked");
@@ -22,9 +25,19 @@ public class TokenValidationMiddleware(RequestDelegate next, HttpClient httpClie
         await next(context);
     }
 
-    private async Task<bool> IsTokenValidAsync(string token)
+    private async Task<bool> IsRevokedTokenValidAsync(string token)
     {
-        const string url = "https://localhost:7074/v1/auth/revoke-token";
+        const string url = $"{BaseUrl}/v1/auth/revoked-token";
+        var request = new HttpRequestMessage(HttpMethod.Get, url);
+        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+        var response = await httpClient.SendAsync(request);
+        return response.IsSuccessStatusCode;
+    }
+
+    private async Task<bool> IsExpiredTokenValidAsync(string token)
+    {
+        const string url = $"{BaseUrl}/v1/auth/expired-token";
         var request = new HttpRequestMessage(HttpMethod.Get, url);
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
