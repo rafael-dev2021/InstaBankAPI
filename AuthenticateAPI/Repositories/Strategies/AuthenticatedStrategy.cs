4fe79c13e51b3ebcf81dadaceeefba6a1b1cf249
Rafael Silva <ainbot6@gmail.com>
2024-07-01 23:35:58 -0300

    refactor: change methods to clearer names and easier-to-understand functions

--- AuthenticateAPI/Repositories/Strategies/AuthenticatedStrategy.cs
+++ AuthenticateAPI/Repositories/Strategies/AuthenticatedStrategy.cs
@@ -1,9 +1,11 @@
-﻿using AuthenticateAPI.Dto.Request;
+﻿using System.Text;
+using AuthenticateAPI.Dto.Request;
 using AuthenticateAPI.Dto.Response;
 using AuthenticateAPI.Models;
 using AuthenticateAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Caching.Distributed;
+using Serilog;
 
 namespace AuthenticateAPI.Repositories.Strategies;
 
@@ -16,8 +18,12 @@ public class AuthenticatedStrategy(SignInManager<User> signInManager, IDistribut
         var loginAttemptsKey = $"{LoginAttemptsKeyPrefix}{request.Email}";
         var loginAttempts = await GetLoginAttemptsAsync(loginAttemptsKey);
 
-        if (loginAttempts >= 5)
+        Log.Information("[AUTHENTICATION] Attempting to authenticate user [{Email}]", request.Email);
+
+        if (loginAttempts >= 3)
         {
+            Log.Warning("[AUTHENTICATION] User [{Email}] account is locked due to multiple failed attempts.",
+                request.Email);
             return new AuthenticatedDtoResponse(false, "Your account is locked. Please contact support.");
         }
 
@@ -27,34 +33,67 @@ public class AuthenticatedStrategy(SignInManager<User> signInManager, IDistribut
         if (result.Succeeded)
         {
             await ResetLoginAttemptsAsync(loginAttemptsKey);
+            Log.Information("[AUTHENTICATION] User [{Email}] successfully authenticated.", request.Email);
             return new AuthenticatedDtoResponse(true, "Login successful.");
         }
 
         await IncrementLoginAttemptsAsync(loginAttemptsKey);
-        
-        var errorMessage = result.IsLockedOut ? "Your account is locked. Please contact support." :
-            "Invalid email or password. Please try again.";
+
+        var errorMessage = result.IsLockedOut
+            ? "Your account is locked. Please contact support."
+            : "Invalid email or password. Please try again.";
+
+        Log.Warning("[AUTHENTICATION] Failed authentication attempt for user [{Email}] with message: [{ErrorMessage}]", request.Email,
+            errorMessage);
         return new AuthenticatedDtoResponse(false, errorMessage);
     }
 
-    private async Task<int> GetLoginAttemptsAsync(string cacheKey)
+    public async Task<int> GetLoginAttemptsAsync(string cacheKey)
     {
-        var attempts = await cache.GetStringAsync(cacheKey);
-        return attempts != null ? int.Parse(attempts) : 0;
-    }
-
-    private async Task IncrementLoginAttemptsAsync(string cacheKey)
-    {
-        var attempts = await GetLoginAttemptsAsync(cacheKey);
-        attempts++;
-        await cache.SetStringAsync(cacheKey, attempts.ToString(), new DistributedCacheEntryOptions
+        try
         {
-            AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
-        });
+            var attempts = await cache.GetStringAsync(cacheKey);
+            return attempts != null ? int.Parse(attempts) : 0;
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "[CACHE] Error retrieving login attempts for cache key [{CacheKey}]", cacheKey);
+            throw;
+        }
     }
 
-    private async Task ResetLoginAttemptsAsync(string cacheKey)
+    public async Task IncrementLoginAttemptsAsync(string cacheKey)
     {
-        await cache.RemoveAsync(cacheKey);
+        try
+        {
+            var attemptsBytes = await cache.GetAsync(cacheKey);
+            var attempts = attemptsBytes != null ? int.Parse(Encoding.UTF8.GetString(attemptsBytes)) : 0;
+            attempts++;
+            await cache.SetStringAsync(cacheKey, attempts.ToString(), new DistributedCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(10)
+            });
+
+            Log.Information("[CACHE] Login attempts incremented for cache key [{CacheKey}]", cacheKey);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "[CACHE] Error incrementing login attempts for cache key [{CacheKey}]", cacheKey);
+            throw;
+        }
+    }
+
+    public async Task ResetLoginAttemptsAsync(string cacheKey)
+    {
+        try
+        {
+            await cache.RemoveAsync(cacheKey);
+            Log.Information("[CACHE] Login attempts reset for cache key [{CacheKey}]", cacheKey);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex, "[CACHE] Error resetting login attempts for cache key [{CacheKey}]", cacheKey);
+            throw;
+        }
     }
 }
--- AuthenticateAPI/Repositories/Strategies/UpdateProfileStrategy.cs
+++ AuthenticateAPI/Repositories/Strategies/UpdateProfileStrategy.cs
@@ -5,71 +5,89 @@ using AuthenticateAPI.Models;
 using AuthenticateAPI.Repositories.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 
 namespace AuthenticateAPI.Repositories.Strategies;
 
 public class UpdateProfileStrategy(UserManager<User> userManager, AppDbContext appDbContext) : IUpdateProfileStrategy
 {
-    private async Task<bool> IsEmailAlreadyUsedByAnotherUser(string email, string userId) =>
-        await appDbContext.Users.AnyAsync(x => x.Email == email && x.Id != userId);
+    public async Task<UpdatedDtoResponse> UpdateProfileAsync(UpdateUserDtoRequest updateUserDtoRequest, string userId)
+    {
+        Log.Information("[PROFILE UPDATE] Attempting to update profile for user [{UserId}]", userId);
 
-    private async Task<bool> IsPhoneNumberAlreadyUsedByAnotherUser(string phone, string userId) =>
-        await appDbContext.Users.AnyAsync(x => x.PhoneNumber == phone && x.Id != userId);
+        var user = await userManager.FindByIdAsync(userId);
+        if (user == null)
+        {
+            Log.Warning("[PROFILE UPDATE] User [{UserId}] not found.", userId);
+            return new UpdatedDtoResponse(false, "User not found");
+        }
 
-    public async Task<List<string>> ValidateAsync(string? email, string? phoneNumber, string userId)
+        var validationErrors = await ValidateUserDetailsAsync(updateUserDtoRequest, user);
+        if (validationErrors.Count != 0)
+        {
+            LogValidationErrors(userId, validationErrors);
+            return new UpdatedDtoResponse(false, string.Join(Environment.NewLine, validationErrors));
+        }
+
+        UpdateUserFields(user, updateUserDtoRequest);
+        var result = await userManager.UpdateAsync(user);
+
+        return result.Succeeded ? LogAndReturnSuccess(userId) : LogAndReturnFailure(userId);
+    }
+
+    private async Task<List<string>> ValidateUserDetailsAsync(UpdateUserDtoRequest updateUserDtoRequest, User user)
     {
         var validationErrors = new List<string>();
 
-        var errors = new Dictionary<Func<Task<bool>>, string>
-        {
-            { () => IsEmailAlreadyUsedByAnotherUser(email!, userId), "Email already used by another user." },
-            { () => IsPhoneNumberAlreadyUsedByAnotherUser(phoneNumber!, userId), "Phone number already used by another user." }
-        };
+        if (updateUserDtoRequest.Email != user.Email &&
+            await IsEmailAlreadyUsedByAnotherUserAsync(updateUserDtoRequest.Email!, user.Id))
+            validationErrors.Add("Email already used by another user.");
 
-        foreach (var error in errors)
-        {
-            if (!string.IsNullOrEmpty(error.Value) && await error.Key())
-            {
-                validationErrors.Add(error.Value);
-            }
-        }
+        if (updateUserDtoRequest.PhoneNumber != user.PhoneNumber &&
+            await IsPhoneNumberAlreadyUsedByAnotherUserAsync(updateUserDtoRequest.PhoneNumber!, user.Id))
+            validationErrors.Add("Phone number already used by another user.");
 
         return validationErrors;
     }
 
-    public async Task<UpdatedDtoResponse> UpdateProfileAsync(UpdateUserDtoRequest updateUserDtoRequest, string userId)
+    private async Task<bool> IsEmailAlreadyUsedByAnotherUserAsync(string email, string userId) =>
+        await appDbContext.Users.AnyAsync(x => x.Email == email && x.Id != userId);
+
+    private async Task<bool> IsPhoneNumberAlreadyUsedByAnotherUserAsync(string phone, string userId) =>
+        await appDbContext.Users.AnyAsync(x => x.PhoneNumber == phone && x.Id != userId);
+
+    private static void UpdateUserFields(User user, UpdateUserDtoRequest updateUserDtoRequest)
     {
-        var user = await userManager.FindByIdAsync(userId);
-
-        var validationErrors = await ValidateAsync(
-            updateUserDtoRequest.Email != user!.Email ? updateUserDtoRequest.Email : null,
-            updateUserDtoRequest.PhoneNumber != user.PhoneNumber ? updateUserDtoRequest.PhoneNumber : null,
-            userId
-        );
-        
-        if (validationErrors.Count > 0)
-        {
-            return new UpdatedDtoResponse(false, string.Join(Environment.NewLine, validationErrors));
-        }
-
         UpdateField(user, (u, v) => u.Email = v, user.Email, updateUserDtoRequest.Email);
         UpdateField(user, (u, v) => u.PhoneNumber = v, user.PhoneNumber, updateUserDtoRequest.PhoneNumber);
         UpdateField(user, (u, v) => u.SetName(v), user.Name, updateUserDtoRequest.Name);
         UpdateField(user, (u, v) => u.SetLastName(v), user.LastName, updateUserDtoRequest.LastName);
-
-
-        var result = await userManager.UpdateAsync(user);
-
-        return result.Succeeded
-            ? new UpdatedDtoResponse(true, "Profile updated successfully.")
-            : new UpdatedDtoResponse(false, "Failed to update profile.");
     }
-    
+
     private static void UpdateField<T>(User user, Action<User, T> setter, T currentValue, T newValue)
     {
-        if (!EqualityComparer<T>.Default.Equals(newValue, default) && !EqualityComparer<T>.Default.Equals(newValue, currentValue))
+        if (!EqualityComparer<T>.Default.Equals(newValue, default) &&
+            !EqualityComparer<T>.Default.Equals(newValue, currentValue))
         {
             setter(user, newValue);
         }
+    }
+
+    private static void LogValidationErrors(string userId, List<string> validationErrors)
+    {
+        Log.Warning("[PROFILE UPDATE] Validation errors for user [{UserId}] with message: [{Errors}]", userId,
+            string.Join(", ", validationErrors));
+    }
+
+    private static UpdatedDtoResponse LogAndReturnSuccess(string userId)
+    {
+        Log.Information("[PROFILE UPDATE] Profile successfully updated for user [{UserId}]", userId);
+        return new UpdatedDtoResponse(true, "Profile updated successfully.");
+    }
+
+    private static UpdatedDtoResponse LogAndReturnFailure(string userId)
+    {
+        Log.Warning("[PROFILE UPDATE] Failed to update profile for user [{UserId}]", userId);
+        return new UpdatedDtoResponse(false, "Failed to update profile.");
     }
 }
