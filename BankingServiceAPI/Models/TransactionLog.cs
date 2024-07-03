c466ac7256456767522ad28f18d0030dca099de4
Rafael Silva <ainbot6@gmail.com>
2024-07-03 01:25:40 -0300

    refactor: change to instantiate new classes

--- BankingServiceAPI/Models/TransactionLog.cs
+++ BankingServiceAPI/Models/TransactionLog.cs
@@ -3,12 +3,36 @@
 public class TransactionLog
 {
     public int Id { get; init; }
-    public int BankTransactionId { get; init; }
-    public BankTransaction? BankTransaction { get; init; }
-    public string? TransactionType { get; init; } 
-    public string? Description { get; init; }
-    public decimal Amount { get; init; } 
-    public int AccountOriginId { get; init; } 
-    public int? AccountDestinationId { get; init; } 
-    public DateTime TransactionDate { get; init; } = DateTime.Now; 
+    public int BankTransactionId { get; private set; }
+    public BankTransaction? BankTransaction { get; private set; }
+    public string? TransactionType { get; private set; }
+    public decimal Amount { get; private set; }
+    public int AccountOriginId { get; private set; }
+    public int? AccountDestinationId { get; private set; }
+    public DateTime TransactionDate { get; private set; } = DateTime.Now;
+    public TransactionDetails? TransactionDetails { get; private set; }
+    public TransactionAudit? TransactionAudit { get; private set; }
+
+    private void SetBankTransactionId(int bankTransactionId) => BankTransactionId = bankTransactionId;
+    public void SetBankTransaction(BankTransaction bankTransaction) => BankTransaction = bankTransaction;
+    private void SetTransactionType(string? transactionType) => TransactionType = transactionType;
+    private void SetAmount(decimal amount) => Amount = amount;
+    private void SetAccountOriginId(int accountOriginId) => AccountOriginId = accountOriginId;
+    private void SetAccountDestinationId(int? accountDestinationId) => AccountDestinationId = accountDestinationId;
+    private void SetTransactionDate(DateTime transactionDate) => TransactionDate = transactionDate;
+    private void SetTransactionDetails(TransactionDetails details) => TransactionDetails = details;
+    private void SetTransactionAudit(TransactionAudit audit) => TransactionAudit = audit;
+
+    public void Configure(int bankTransactionId, string transactionType, decimal amount, int accountOriginId,
+        int? accountDestinationId, DateTime transactionDate, TransactionDetails details, TransactionAudit audit)
+    {
+        SetBankTransactionId(bankTransactionId);
+        SetTransactionType(transactionType);
+        SetAmount(amount);
+        SetAccountOriginId(accountOriginId);
+        SetAccountDestinationId(accountDestinationId);
+        SetTransactionDate(transactionDate);
+        SetTransactionDetails(details);
+        SetTransactionAudit(audit);
+    }
 }
