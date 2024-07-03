a566b5c2e269f213410a12af649c0f859d24db08
Rafael Silva <ainbot6@gmail.com>
2024-07-03 01:20:46 -0300

    feat: add new models

---
+++ BankingServiceAPI/Models/TransactionAudit.cs
@@ -0,0 +1,29 @@
+﻿namespace BankingServiceAPI.Models;
+
+public class TransactionAudit
+{
+    public string? InitiatedBy { get; private set; }
+    public string? ApprovedBy { get; private set; }
+    public string? IpAddress { get; private set; }
+    public string? DeviceId { get; private set; }
+    public string? Location { get; private set; }
+    public DateTime Timestamp { get; private set; }
+
+    public void SetInitiatedBy(string? initiatedBy) => InitiatedBy = initiatedBy;
+    public void SetApprovedBy(string? approvedBy) => ApprovedBy = approvedBy;
+    public void SetIpAddress(string? ipAddress) => IpAddress = ipAddress;
+    public void SetDeviceId(string? deviceId) => DeviceId = deviceId;
+    public void SetLocation(string? location) => Location = location;
+    public void SetTimestamp(DateTime timestamp) => Timestamp = timestamp;
+
+    public void Configure(string? initiatedBy, string? approvedBy, string? ipAddress, string? deviceId,
+        string? location, DateTime timestamp)
+    {
+        SetInitiatedBy(initiatedBy);
+        SetApprovedBy(approvedBy);
+        SetIpAddress(ipAddress);
+        SetDeviceId(deviceId);
+        SetLocation(location);
+        SetTimestamp(timestamp);
+    }
+}
---
+++ BankingServiceAPI/Models/TransactionDetails.cs
@@ -0,0 +1,29 @@
+﻿namespace BankingServiceAPI.Models;
+
+public class TransactionDetails
+{
+    public string? TransactionStatus { get; private set; }
+    public string? Description { get; private set; }
+    public string? Remarks { get; private set; }
+    public string? TransactionReference { get; private set; }
+    public string? Channel { get; private set; }
+    public string? ErrorDetails { get; private set; }
+
+    public void SetTransactionStatus(string? transactionStatus) => TransactionStatus = transactionStatus;
+    public void SetDescription(string? description) => Description = description;
+    public void SetRemarks(string? remarks) => Remarks = remarks;
+    public void SetTransactionReference(string? transactionReference) => TransactionReference = transactionReference;
+    public void SetChannel(string? channel) => Channel = channel;
+    public void SetErrorDetails(string? errorDetails) => ErrorDetails = errorDetails;
+
+    public void Configure(string? transactionStatus, string? description, string? remarks, string? transactionReference,
+        string? channel, string? errorDetails)
+    {
+        SetTransactionStatus(transactionStatus);
+        SetDescription(description);
+        SetRemarks(remarks);
+        SetTransactionReference(transactionReference);
+        SetChannel(channel);
+        SetErrorDetails(errorDetails);
+    }
+}
